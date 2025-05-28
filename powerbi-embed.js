5ce31725b860eaacb70545a833fe3ada2b0d6e51
charleschale <46422425+charleschale@users.noreply.github.com>
2025-05-28 09:55:42 -0400

    Add files via upload

---
+++ powerbi-embed.js
@@ -0,0 +1,33 @@
+import { defineCustomElement } from '@wix/custom-elements';
+
+defineCustomElement('PowerBIEmbed', () => {
+  const container = document.createElement('div');
+  container.id = 'reportContainer';
+  container.style.width = '100%';
+  container.style.height = '100%';
+
+  const sdkScript = document.createElement('script');
+  sdkScript.src = 'https://cdn.jsdelivr.net/npm/powerbi-client@2.21.0/dist/powerbi.min.js';
+  sdkScript.onload = () => {
+    fetch("https://powerbi-token-server.onrender.com/getEmbedToken")
+      .then(res => res.json())
+      .then(data => {
+        const models = window['powerbi-client'].models;
+        const config = {
+          type: 'report',
+          id: data.reportId,
+          embedUrl: data.embedUrl,
+          accessToken: data.token,
+          tokenType: models.TokenType.Embed,
+          settings: {
+            filterPaneEnabled: false,
+            navContentPaneEnabled: true
+          }
+        };
+        window.powerbi.embed(container, config);
+      });
+  };
+
+  container.appendChild(sdkScript);
+  return container;
+});
