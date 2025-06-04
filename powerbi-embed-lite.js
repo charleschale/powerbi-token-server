3b7ebcd83516880d4ccb09938ca678117e5820e0
charleschale <46422425+charleschale@users.noreply.github.com>
2025-06-04 19:18:48 -0400

    Delete powerbi-embed-lite.js

--- powerbi-embed-lite.js
+++
@@ -1,8 +0,0 @@
-defineCustomElement('powerbi-embed', () => {
-  const el = document.createElement('div');
-  el.innerText = '✅ Custom Element Loaded Successfully';
-  el.style.background = '#e0ffe0';
-  el.style.padding = '20px';
-  el.style.fontSize = '20px';
-  return el;
-});
